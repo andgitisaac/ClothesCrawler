df68b1ad218e2ab6920406dc1cfa7befa558e939
andgitisaac <andgitisaac@gmail.com>
2018-01-15 23:04:42 +0800

    grab urls of NET

--- NETcrawlers.py
+++ NETcrawlers.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import time
 from urllib.request import Request, urlopen, urlretrieve, build_opener, install_opener
 from bs4 import BeautifulSoup as bs
 
@@ -20,18 +22,14 @@ def get_pic_url(soup):
     pics_url = []
     for pic_class in pics_class:
         try:
-            pics_url.append(pic_class.find('img')['src'])
+            urlStr = pic_class.find('img')['src'].split('?')[0]
+            if bool(re.search(r'(?:hair|warmheat|warmtech|/001.jpg|LONEY|WOOL|sp|BonhuiUy|NT|．)', urlStr)):
+                # These keywords in the url means this photo is useless
+                continue
+            pics_url.append(urlStr)
         except TypeError:
             pass
     return pics_url
-
-def download_image(url_list, count):
-    for index, url in enumerate(url_list):
-        filename = str(count) + "_" + str(index) + ".jpg"
-        opener = build_opener()
-        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
-        install_opener(opener)
-        urlretrieve(url, filename)
 
 def collect_urls(url_list, filename, PID, IID):
     with open(filename, 'a') as file:
@@ -42,14 +40,23 @@ def collect_urls(url_list, filename, PID, IID):
             file.write(url)
             file.write('\n')
             IID += 1
+    return IID
+
+# def download_image(url_list, count):
+#     for index, url in enumerate(url_list):
+#         filename = str(count) + "_" + str(index) + ".jpg"
+#         opener = build_opener()
+#         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
+#         install_opener(opener)
+#         urlretrieve(url, filename)
 
 if __name__ == '__main__':    
-    page = [60, 15]  # Women:60, Men:15
+    pageNum = [60, 15]  # Women:60, Men:15
     url_filename = ['NETwomen_urls', 'NETmen_urls']
     base_url = ['https://www.net-fashion.net/category/998', 'https://www.net-fashion.net/category/999']
     
-    for iter in range(2):
-        pagecount = page[iter]
+    for iter in range(1, 2):
+        pagecount = pageNum[iter]
         filename = url_filename[iter]
         suffix = [''] + ['/' + str(i) for i in range(2, pagecount+1) ]
         identityCount, productCount = 0, 0
@@ -63,10 +70,8 @@ if __name__ == '__main__':
                 product_page = get_web_page(product_url)
                 product_soup = bs(product_page, 'html.parser')
                 pics_url = get_pic_url(product_soup)        
-                collect_urls(pics_url, filename, productCount, identityCount)            
+                identityCount = collect_urls(pics_url, filename, productCount, identityCount)            
                 productCount += 1
-
-
             print("page: ", i)
-
+            time.sleep(1)
 
