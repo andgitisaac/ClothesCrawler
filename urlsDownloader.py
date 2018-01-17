8706fcbca48b8b66f0574d591df6af181c0cce4a
andgitisaac <andgitisaac@gmail.com>
2018-01-17 11:42:20 +0800

    use for life8 casual

--- urlsDownloader.py
+++ urlsDownloader.py
@@ -14,7 +14,7 @@ def download_image(url, filename, file_in, lineNo):
 
     try:
         raw = requests.get(url, headers=hdr)
-        with open(os.path.join(os.getcwd(), 'PHOTO_life8', file_out), 'wb') as image:
+        with open(os.path.join(os.getcwd(), 'PHOTO_life8_2', file_out), 'wb') as image:
             image.write(raw.content)
         time.sleep(0.5) # Maybe wait for a longer time until connecting to the url?
         return False
@@ -32,14 +32,14 @@ def download_image(url, filename, file_in, lineNo):
         return True
 
 if __name__ == '__main__':
-    file_in = 'life8_causual_urls'
+    file_in = 'life8_formal_urls'
     lineNo = 0
 
     f = open(file_in, 'r')
     for line in f:
-        if lineNo < 1906:
-            lineNo += 1
-            continue
+        # if lineNo < 1906:
+        #     lineNo += 1
+        #     continue
         # Since last time it fails at line 1906
 
         file_out, url = line.strip('\n').split()
@@ -47,16 +47,17 @@ if __name__ == '__main__':
         exceptionRaise = download_image(url, file_out, file_in, lineNo)
             
         if exceptionRaise:
-            print("Exception Raise at line {}. Halt for 1800 secs".format(lineNo))
+            print("Exception Raise at line {}. Halt from {} for 1800 secs".format(lineNo, time.strftime("%H:%M:%S", time.localtime())))
             time.sleep(1800)
         elif (lineNo % 100) == 0 and lineNo != 0: # Sleep for an hour for every 100 photos
-            print("At line {}. Halt for 1800 secs".format(lineNo))
-            time.sleep(1200)
-        elif (lineNo % 10) == 0 and lineNo != 0: # Sleep for a while for every 10 photos
+            print("At line {}. Halt from {}  for 600 secs".format(lineNo, time.strftime("%H:%M:%S", time.localtime())))
+            time.sleep(600)
+        elif (lineNo % 20) == 0 and lineNo != 0: # Sleep for a while for every 20 photos
             var = random.randrange(30, 60)
             print("At line {}. Halt for {} secs".format(lineNo, var))
             time.sleep(var)
         lineNo += 1
     f.close()
+    print('Finished!')
             
         
